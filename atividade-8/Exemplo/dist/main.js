82bab7df9511ba3cf2a27067b6dff8d5f1a09911
BrunoBehrmann <102628678+BrunoBehrmann@users.noreply.github.com>
2022-09-03 10:49:27 -0300

    Delete atividade-8/Exemplo directory

--- atividade-8/Exemplo/dist/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Atendimento_1 = require("./Atendimento");
-const Recepcionista_1 = require("./Recepcionista");
-var r1 = new Recepcionista_1.Recepcionista(1212, 'Juca', 20, true, 'A');
-var a1 = new Atendimento_1.Atendimento(1);
-console.log("teste");
